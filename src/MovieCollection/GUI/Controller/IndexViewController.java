08bdce0cfea3c91ef5e02e1c5ce13d14c3252b9b
TheDarkSide1992 <Jenscp171@gmail.com>
2023-01-03 10:33:57 +0100

    GitIgnore file and Index FXML

---
+++ src/MovieCollection/GUI/Controller/IndexViewController.java
@@ -0,0 +1,13 @@
+package MovieCollection.GUI.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class IndexViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+}
--- src/MovieCollection/GUI/Main.java
+++ src/MovieCollection/GUI/Main.java
@@ -1,7 +1,22 @@
 package MovieCollection.GUI;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("View/indexView.fxml"));
+        primaryStage.setTitle("Movie Collection");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 }
