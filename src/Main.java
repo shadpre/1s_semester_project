ff2e73f9ab9ce020eda383a243d2228ff04940ad
Klavs Tranborg <klavs@tranborg.eu>
2023-01-04 12:20:24 +0100

    Created DB connector and instanciated it in main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import MovieCollection.BLL.DatabaseConnector;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        //System.out.println("Hello world!");
+        var url =  Main.class.getResource("config.properties");
+
+        Properties props = new Properties();
+        if(url != null)
+            try(InputStream input = url.openStream()) {
+                props.load(input);
+            }
+
+        var dbIp = props.getProperty("DB_IP");
+        var dbPort = Integer.parseInt(props.getProperty("DB_PORT"));
+        var dbName= props.getProperty("DB_NAME");
+        var dbUsername = props.getProperty("DB_USERNAME");
+        var dbPassword = props.getProperty("DB_PASSWORD");
+
+        DatabaseConnector.init(dbIp, dbPort, dbName, dbUsername, dbPassword);
+
         launch(args);
     }
 
--- src/MovieCollection/BLL/DatabaseConnector.java
+++ src/MovieCollection/BLL/DatabaseConnector.java
@@ -1,4 +1,57 @@
 package MovieCollection.BLL;
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DatabaseConnector {
+    private static DatabaseConnector instance;
+    private SQLServerDataSource source;
+
+    private DatabaseConnector(String ip, int port, String dbName, String username, String password){
+        source = new SQLServerDataSource();
+        source.setServerName(ip);
+        source.setPortNumber(port);
+        source.setUser(username);
+        source.setPassword(password);
+        source.setDatabaseName(dbName);
+        source.setTrustServerCertificate(true);
+
+        if(!isConnectionWorking()) {
+            System.exit(-1);
+        }
+    }
+
+    public boolean isConnectionWorking() {
+        try {
+            if (getConnection() != null && !getConnection().isClosed()) {
+                return true;
+            } else {
+                System.err.println("No connection to database!");
+            }
+        } catch (SQLException e) {
+            System.err.println("No connection to database!");
+        }
+        return false;
+    }
+
+    public static void init(String ip, int port, String dbName, String username, String password) {
+        if(instance != null) return;
+
+        instance = new DatabaseConnector(ip, port, dbName, username, password);
+    }
+
+    public static DatabaseConnector getInstance() {
+        if (instance == null) {
+            System.err.println("Please initialize DatabaseConnectionHandler.java before using it!");
+            System.exit(-1);
+
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return source.getConnection();
+    }
 }
