4db24fc1335c208bf349ee8bea8c6759ff810926
TheDarkSide1992 <Jenscp171@gmail.com>
2023-01-05 09:38:51 +0100

    All Dao Files Created

---
+++ src/MovieCollection/DAL/MovieDAO.java
@@ -0,0 +1,27 @@
+package MovieCollection.DAL;
+
+import MovieCollection.BE.Movie;
+
+import java.util.ArrayList;
+
+public class MovieDAO implements IMovieDAO{
+    @Override
+    public ArrayList<Movie> getAllMovies() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteMovie(int iD) throws Exception {
+
+    }
+
+    @Override
+    public void createMovie(Movie movie) throws Exception {
+
+    }
+
+    @Override
+    public void updateMovie(Movie movie) throws Exception {
+
+    }
+}
---
+++ src/MovieCollection/DAL/SubjectDAO.java
@@ -0,0 +1,27 @@
+package MovieCollection.DAL;
+
+import MovieCollection.BE.Subject;
+
+import java.util.ArrayList;
+
+public class SubjectDAO implements ISubjectDAO{
+    @Override
+    public ArrayList<Subject> getAllSubjects() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteSubject(int iD) throws Exception {
+
+    }
+
+    @Override
+    public void createSubject(Subject subject) throws Exception {
+
+    }
+
+    @Override
+    public void updateSubject(Subject subject) throws Exception {
+
+    }
+}
