b7370a89884478a0c911985c7e9e0fce16632dcf
Klavs Tranborg <klavs@tranborg.eu>
2023-01-05 09:34:53 +0100

    CategoryDAO.java

--- src/MovieCollection/DAL/ICategoryDAO.java
+++ src/MovieCollection/DAL/ICategoryDAO.java
@@ -1,4 +1,13 @@
 package MovieCollection.DAL;
 
+import MovieCollection.BE.Category;
+
+import java.util.ArrayList;
+
 public interface ICategoryDAO {
+    int createCategory(String category) throws Exception;
+    Category getCategory(int id) throws Exception;
+    ArrayList<Category> getAllCategories() throws Exception;
+    int editCategory(Category category) throws Exception;
+    void deleteCategory(int id) throws Exception;
 }
