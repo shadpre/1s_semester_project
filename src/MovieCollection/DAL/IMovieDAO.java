2476a21800d13981d46fff4b3d98574be5d538ab
TheDarkSide1992 <Jenscp171@gmail.com>
2023-01-05 09:22:28 +0100

    IMovie Interface

--- src/MovieCollection/DAL/IMovieDAO.java
+++ src/MovieCollection/DAL/IMovieDAO.java
@@ -1,4 +1,12 @@
 package MovieCollection.DAL;
 
+import MovieCollection.BE.Movie;
+
+import java.util.ArrayList;
+
 public interface IMovieDAO {
+   ArrayList<Movie> getAllMovies() throws Exception;
+   void deleteMovie(int iD) throws Exception;
+   void createMovie(Movie movie) throws Exception;
+   Movie updateMovie(Movie movie) throws Exception;
 }
