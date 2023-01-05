c4ba29a5fe3521408a37d16c1ce7bc90ce7d7c32
Klavs Tranborg <klavs@tranborg.eu>
2023-01-05 09:50:14 +0100

    CategoryDAO.java

--- src/MovieCollection/DAL/CategoryDAO.java
+++ src/MovieCollection/DAL/CategoryDAO.java
@@ -1,4 +1,32 @@
 package MovieCollection.DAL;
 
+import MovieCollection.BE.Category;
+
+import java.util.ArrayList;
+
 public class CategoryDAO implements ICategoryDAO{
+    @Override
+    public int createCategory(String category) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public Category getCategory(int id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Category> getAllCategories() throws Exception {
+        return null;
+    }
+
+    @Override
+    public int editCategory(Category category) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public void deleteCategory(int id) throws Exception {
+
+    }
 }
