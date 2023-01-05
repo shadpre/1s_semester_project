a311a014d6b9adcada04fc66bba550e8bb3047da
Klavs Tranborg <klavs@tranborg.eu>
2023-01-05 10:49:45 +0100

    CategoryDAO.java

--- src/MovieCollection/DAL/CategoryDAO.java
+++ src/MovieCollection/DAL/CategoryDAO.java
@@ -1,16 +1,31 @@
 package MovieCollection.DAL;
 
 import MovieCollection.BE.Category;
+import MovieCollection.BLL.DatabaseConnector;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class CategoryDAO implements ICategoryDAO{
     @Override
     public int createCategory(String category) throws Exception {
 
-        //TODO create create category
+        try (Connection connection = DatabaseConnector.getInstance().getConnection()){
+            String query = "insert into Category (Name) Values (?)";
 
-        return 0;
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, category);
+
+            var resultSet = statement.getGeneratedKeys();
+            return resultSet.getInt(1);
+
+
+        }
+        catch (Exception ex){
+            throw ex;
+        }
     }
 
     @Override
